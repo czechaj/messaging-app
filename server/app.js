5e0d5502844abd34da17ac25faeb31e950557950
cze <cze_14@outlook.com>
2022-06-03 23:26:12 +0300

    working on adding express

--- server/app.js
+++ server/app.js
@@ -1,12 +1,4 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-//This will create a middleware.
-//When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-const io = require("socket.io")(process.env.PORT || 5000, {
+const io = require("socket.io")(5000, {
   cors: {
     origin: "*",
   },
